38b8351335d47de56273d415df25228624208171
mariacamilo <maria.dom.camilo@gmail.com>
2021-05-13 18:11:26 +0100

    desafios

--- Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
@@ -2,23 +2,37 @@ package pt.upacademy.desafios.desafio1;
 
 public class Desafio {
 
+	int i = 0;
+	int tres = 0;
+	int cinco = 0;
+
 	public Desafio(int i) {
+		this.i = i;
 		// TODO Auto-generated constructor stub
+
 	}
 
 	public void run() {
+		for (int n = 1; n <= i; n++) {
+			if (n % 3 == 0) {
+				tres++;
+			}
+			if (n % 5 == 0) {
+				cinco++;
+			}
+		}
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public int getBig3Count() {
 		// TODO Auto-generated method stub
-		return 0;
+		return tres;
 	}
 
 	public int getBig5Count() {
 		// TODO Auto-generated method stub
-		return 0;
+		return cinco;
 	}
 
 }
--- Examples/src/pt/upacademy/desafios/desafio5/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio5/Desafio.java
@@ -3,7 +3,10 @@ package pt.upacademy.desafios.desafio5;
 public class Desafio {
 
 	public static int countDays(String string, String string2) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("As datas são:" + string + string2);
 		return 0;
 	}
 
